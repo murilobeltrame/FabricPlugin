f9cd369e4e5ce2bc0bb44db480ce4dbc939e7d81
Murilo Beltrame <murilobeltrame@gmail.com>
2019-10-22 09:57:03 -0300

    changed build phase script to work with pods

--- hooks/lib/ios-helper.js
+++ hooks/lib/ios-helper.js
@@ -28,7 +28,8 @@ module.exports = {
         xcodeProject.parseSync();
 
         // Build the body of the script to be executed during the build phase.
-        var script = '"' + '\\"${SRCROOT}\\"' + "/\\\"" + utilities.getAppName(context) + "\\\"/Plugins/cordova-fabric-plugin/Fabric.framework/run " + pluginConfig.apiKey + " " + pluginConfig.apiSecret + '"';
+        // var script = '"' + '\\"${SRCROOT}\\"' + "/\\\"" + utilities.getAppName(context) + "\\\"/Plugins/cordova-fabric-plugin/Fabric.framework/run " + pluginConfig.apiKey + " " + pluginConfig.apiSecret + '"';
+        var script = '"' + '\\"${PODS_ROOT}\\"' + "\\\"/Fabric/run " + pluginConfig.apiKey + " " + pluginConfig.apiSecret + '"';
 
         // Generate a unique ID for our new build phase.
         var id = xcodeProject.generateUuid();
